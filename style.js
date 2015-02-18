5a9c523f460ee668750836324dfd67d37a05f206
Melissa Gaines <gaines.melissa23@gmail.com>
2015-02-17 21:06:57 -0500

    first commit

---
+++ style.js
@@ -0,0 +1,22 @@
+
+function GetClock(){
+var refresh=1000;
+var d=new Date();
+var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),nmil=d.getMilliseconds();
+
+     if(nhour==0){ap=" AM";nhour=12;}
+else if(nhour<12){ap=" AM";}
+else if(nhour==12){ap=" PM";}
+else if(nhour>12){ap=" PM";nhour-=12;}
+
+if(nmin<=9) nmin="0"+nmin;
+if(nsec<=9) nsec="0"+nsec;
+if(nmil<=9) nmil="0"+nmil;
+
+document.getElementById('clockbox').innerHTML=""+nhour+":"+nmin+":"+nsec+":"+nmil+ap+"";
+}
+
+window.onload=function(){
+GetClock();
+setInterval(GetClock,1000);
+}
